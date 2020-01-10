ab52041d851c4388226a80b6dcfc118512bce0f0
Juan Luis <juaperbar2@alum.us.es>
2020-01-10 22:20:36 +0100

    refactor: Added name to voting/url.py
    Added the atribte name to url.py in voting
    module in order to identify better the
    views
    
    References: #33

--- decide/voting/urls.py
+++ decide/voting/urls.py
@@ -6,7 +6,7 @@ urlpatterns = [
     path('', views.VotingView.as_view(), name='voting'),
     path('<int:voting_id>/', views.VotingUpdate.as_view(), name='voting'),
     path('user/', views.VotingsPerUser.as_view(), name='votingsPerUser'),
-    re_path(r'^politicalparty/$', views.PoliticalPartyList.as_view()),
-    re_path(r'^politicalparty/(?P<pk>\d+)/$',views.PoliticalPartyDetail.as_view()),
+    re_path(r'^politicalparty/$', views.PoliticalPartyList.as_view(), name='get_post_politicalparty'),
+    re_path(r'^politicalparty/(?P<pk>\d+)/$',views.PoliticalPartyDetail.as_view(), name='get_delete_update_politicalparty'),
 
 ]
