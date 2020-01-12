948a64fdc219f93f5d89561143c4158e1cfa266d
Aguza5 <43997459+Aguza5@users.noreply.github.com>
2020-01-12 23:01:03 +0100

    config: changed keybit for allow voting

--- decide/decide/settings.py
+++ decide/decide/settings.py
@@ -175,7 +175,7 @@ TEST_RUNNER = 'django_nose.NoseTestSuiteRunner'
 STATIC_URL = '/static/'
 
 # number of bits for the key, all auths should use the same number of bits
-KEYBITS = 161
+KEYBITS = 32
 
 # Versioning
 ALLOWED_VERSIONS = ['v1', 'v2']
